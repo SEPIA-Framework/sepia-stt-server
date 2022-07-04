63f72dc425ae0d2177d11cde5f4232b7c6c22e29
Florian Quirin <info@bytemind.de>
2022-07-04 22:42:12 +0200

    fixed Dockerfile for 'dynamic' engine

--- engines/dynamic/test_basic_server.sh
+++ engines/dynamic/test_basic_server.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-IMAGE_TAG=latest
-sudo docker run --rm --name=sepia-stt-vosk -p 20741:20741 -it sepia/stt-server:$IMAGE_TAG /bin/bash
+IMAGE_TAG=
+version=
+if [ -n "$1" ]; then
+	version=$1
+else
+	echo "Please specify the version, e.g. 'v1.0.0'"
+	exit
+fi
+if [ -n "$(uname -m | grep aarch64)" ]; then
+	IMAGE_TAG="dynamic_${version}_aarch64"
+elif [ -n "$(uname -m | grep armv7l)" ]; then
+	IMAGE_TAG="dynamic_${version}_armv7l"
+else
+	# NOTE: x86 32bit build not supported atm
+	IMAGE_TAG="dynamic_${version}_amd64"
+fi
+sudo docker run --rm --name=sepia-stt-vosk -p 20741:20741 -it sepia/stt-server:"$IMAGE_TAG" /bin/bash
 #-v share:/home/admin/share
--- engines/dynamic/test_terminal_with_vol.sh
+++ engines/dynamic/test_terminal_with_vol.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-IMAGE_TAG=latest
+IMAGE_TAG=
+version=
+if [ -n "$1" ]; then
+	version=$1
+else
+	echo "Please specify the version, e.g. 'v1.0.0'"
+	exit
+fi
+if [ -n "$(uname -m | grep aarch64)" ]; then
+	IMAGE_TAG="dynamic_${version}_aarch64"
+elif [ -n "$(uname -m | grep armv7l)" ]; then
+	IMAGE_TAG="dynamic_${version}_armv7l"
+else
+	# NOTE: x86 32bit build not supported atm
+	IMAGE_TAG="dynamic_${version}_amd64"
+fi
 HOST_MODELS="$(realpath ~)/stt/models"
 HOST_SHARE="$(realpath ~)/stt/share"
 mkdir -p "$HOST_MODELS"
@@ -8,5 +23,5 @@ sudo docker run --rm --name=sepia-stt -p 20741:20741 -it \
 	-v "$HOST_MODELS":/home/admin/sepia-stt/models/my \
 	-v "$HOST_SHARE":/home/admin/sepia-stt/share \
 	--env SEPIA_STT_SETTINGS=/home/admin/sepia-stt/share/my.conf \
-	sepia/stt-server:$IMAGE_TAG \
+	sepia/stt-server:"$IMAGE_TAG" \
 	/bin/bash
